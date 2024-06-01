95b327267c113e487c64843e4fd75497af405e48
Diana Vashchilina <vohnenna@gmail.com>
2024-06-01 11:21:34 +0300

    Update Docusaurus configuration for GitHub pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,91 +8,91 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "JS шпора",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/logo.png",
+	title: "JS шпора",
+	tagline: "Dinosaurs are cool",
+	favicon: "img/logo.png",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+	// Set the production url of your site here
+	url: "https://your-docusaurus-site.example.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/js_cheatsheet_ua/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "luiqor", // Usually your GitHub org/user name.
+	projectName: "js_cheetsheet_ua", // Usually your repo name.
+	trailingSlash: false,
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "uk",
+		locales: ["uk"],
+	},
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "uk",
-    locales: ["uk"],
-  },
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: "./sidebars.js",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/luiqor/js_cheetsheet_ua",
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/luiqor/js_cheetsheet_ua",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			}),
+		],
+	],
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/luiqor/js_cheetsheet_ua",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/luiqor/js_cheetsheet_ua",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
-      navbar: {
-        title: "JS шпора",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/logo.png",
-        },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Tutorial",
-          },
-          {
-            href: "https://github.com/luiqor/js_cheetsheet_ua",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [],
-        copyright: `Copyright © ${new Date().getFullYear()} js cheetsheet ua, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			// Replace with your project's social card
+			image: "img/docusaurus-social-card.jpg",
+			navbar: {
+				title: "JS шпора",
+				logo: {
+					alt: "My Site Logo",
+					src: "img/logo.png",
+				},
+				items: [
+					{
+						type: "docSidebar",
+						sidebarId: "tutorialSidebar",
+						position: "left",
+						label: "Tutorial",
+					},
+					{
+						href: "https://github.com/luiqor/js_cheetsheet_ua",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [],
+				copyright: `Copyright © ${new Date().getFullYear()} js cheetsheet ua, Inc. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
+			},
+		}),
 };
 
 export default config;
